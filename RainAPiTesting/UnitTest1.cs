52bf3bcfa7e9b0d9ac190d9c7530110b47dc8310
Azka Gilani <azka.gilani@gmail.com>
2024-08-14 22:34:56 +0100

    Initialise project

---
+++ RainAPiTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RainAPiTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
